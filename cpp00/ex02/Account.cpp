e26c8287974368f9d4af0b9fdf2b1363dfdfe838
lizafotkaet <elizavetabarbashh@gmail.com>
2026-05-31 22:31:29 +0200

    ex02 (start)

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -1,2 +1,35 @@
 #include "Account.hpp"
 
+Account::Account( int initial_deposit ){
+
+}
+
+Account::~Account( void ){
+
+}
+
+static void	_displayTimestamp( void ){
+
+};
+
+static int	Account::getNbAccounts( void ){
+
+};
+static int	Account::getTotalAmount( void ){
+
+}
+static int	Account::getNbDeposits( void ){
+
+}
+static int	Account::getNbWithdrawals( void ){
+
+};
+static void	Account::displayAccountsInfos( void ){
+
+};
+
+
+void	Account::makeDeposit( int deposit );
+bool	Account::makeWithdrawal( int withdrawal );
+int		Account::checkAmount( void ) const;
+void	Account::displayStatus( void ) const;
