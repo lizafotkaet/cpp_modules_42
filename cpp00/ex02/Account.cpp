a3169685dfee8ac8525f86bf1e19dfd396c81939
lizafotkaet <elizavetabarbashh@gmail.com>
2026-05-31 23:55:55 +0200

    ex02 (in progress)

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -1,7 +1,12 @@
 #include "Account.hpp"
+#include <iostream>
+#include <ctime>
+#include <iomanip>
 
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
 
 Account::Account( int initial_deposit ) : _nbDeposits(), _nbWithdrawals(), _amount(initial_deposit){
 
@@ -9,17 +14,30 @@ Account::Account( int initial_deposit ) : _nbDeposits(), _nbWithdrawals(), _amou
     _totalAmount += _amount;
 
     ::_displayTimestamp();
-    std::cout << "index: " << _accountIndex << ";amount: " << _amount;
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created\n";
 }
 
 Account::~Account( void ){
-
-
-
+//[19920104_091532] index:0;amount:47;closed
+    ::_displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed\n";
+    _nbAccounts--;
+    _totalAmount -= _amount;
+    
 };
 
 static void	_displayTimestamp( void ){
+    std::time_t now = std::time(nullptr);
+    std::tm* timeinfo = std::localtime(&now);
+    std::cout << "[";
+    std::cout << std::put_time(timeinfo, "%Y%m%d_%H%M%S");
+    std::cout << "] ";
 
+    // std::time_t now = std::time(nullptr);
+    // std::tm* timeinfo = std::localtime(&now);
+    // char buffer[20];
+    // std::strftime(buffer, sizeof(buffer), "%Y%m%d_%H%M%S", timeinfo);
+    // std::cout << "[" << buffer << "] ";
 };
 
 static int	Account::getNbAccounts( void ){
