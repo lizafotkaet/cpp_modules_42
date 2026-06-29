d692b85ece2992f6acd955ae9f1c9f9d76200f57
Elizaveta Barbash <ebarbash@f0r4s16.codam.nl>
2026-06-29 13:56:05 +0200

    cpp01 ex04 in progress

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 
 #include <fstream>
+#include <sstream>
 #include <iostream>
 #include <string>
 
-int main() {
-    std::ifstream file("example.txt");
+std::string replace_text(std::ifstream& inputFile, std::string& s1, std::string& s2)
+{
+	std::stringstream	buffer;
+	buffer << inputFile.rdbuf(); //"read buffer" method that returns a pointer to the underlying stream buffer
+    std::string content = buffer.str();  //.str gives you the string stored inside the stream (extract all as one sings string)
+    size_t pos = 0;
+    while ((pos = content.find(s1, pos)) != std::string::npos) //.find search for substring inside another string.. std::string::npos -- "special "not found"" for .find
+    {
+        content.erase(pos, s1.length()); //.erase to erase [pos - s1.lenght()] index from string
+        content.insert(pos, s2); //.insert to add s2 to index pos
+        pos += s2.length();
+    }
+    return content;
+}
+
+int main(int argc, char** argv) {
+
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <input_file>\n";
+        return 1;
+    }
+
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+
+    std::ifstream file(argv[1]);
 
     // Always check that the file actually opened
     if (!file.is_open()) {
@@ -13,19 +38,16 @@ int main() {
         return 1;
     }
 
+    std::string replaced_content = replace_text(file, s1, s2);
+    file.close();  // optional — happens automatically when 'file' goes out of scope
+
     // Read line by line
     std::string line;
     while (std::getline(file, line)) {
         std::cout << line << '\n';
     }
 
-    file.close();  // optional — happens automatically when 'file' goes out of scope
+    std::ofstream outputFile(std::string(argv[1]) + ".replace");
+
     return 0;
-}
-
-int main(int argc, char** argv){
-    if (argc != 4)
-        return (1);
-
-    
 }
