05de0ffa6e8cce95f6acfc5db5d6f033e7706f48
Elizaveta Barbash <ebarbash@f0r4s16.codam.nl>
2026-06-29 14:20:11 +0200

    cpp01 ex04 done

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #include <fstream>
 #include <sstream>
 #include <iostream>
@@ -8,13 +6,13 @@
 std::string replace_text(std::ifstream& inputFile, std::string& s1, std::string& s2)
 {
 	std::stringstream	buffer;
-	buffer << inputFile.rdbuf(); //"read buffer" method that returns a pointer to the underlying stream buffer
-    std::string content = buffer.str();  //.str gives you the string stored inside the stream (extract all as one sings string)
+	buffer << inputFile.rdbuf();
+    std::string content = buffer.str();
     size_t pos = 0;
-    while ((pos = content.find(s1, pos)) != std::string::npos) //.find search for substring inside another string.. std::string::npos -- "special "not found"" for .find
+    while ((pos = content.find(s1, pos)) != std::string::npos)
     {
-        content.erase(pos, s1.length()); //.erase to erase [pos - s1.lenght()] index from string
-        content.insert(pos, s2); //.insert to add s2 to index pos
+        content.erase(pos, s1.length()); 
+        content.insert(pos, s2);
         pos += s2.length();
     }
     return content;
@@ -23,31 +21,38 @@ std::string replace_text(std::ifstream& inputFile, std::string& s1, std::string&
 int main(int argc, char** argv) {
 
     if (argc != 4) {
-        std::cerr << "Usage: " << argv[0] << " <input_file>\n";
+        std::cerr << "Error: Invalid number of arguments\n";
+        std::cerr << "Usage: " << argv[0] << " <input_file> <search_string> <replacement_string>\n";
         return 1;
     }
 
     std::string s1 = argv[2];
     std::string s2 = argv[3];
 
+    
     std::ifstream file(argv[1]);
-
-    // Always check that the file actually opened
+    
+    // !!! check that the file actually opened
     if (!file.is_open()) {
-        std::cerr << "Could not open the file\n";
+        std::cerr << "Error: could not open the file\n";
+        return 1;
+    }
+    
+    if (s1.empty()) {
+        std::cerr << "Error: s1 must not be empty\n";
         return 1;
     }
 
     std::string replaced_content = replace_text(file, s1, s2);
-    file.close();  // optional — happens automatically when 'file' goes out of scope
-
-    // Read line by line
-    std::string line;
-    while (std::getline(file, line)) {
-        std::cout << line << '\n';
-    }
+    file.close();  // unnecessary — happens automatically when 'file' goes out of scope
 
     std::ofstream outputFile(std::string(argv[1]) + ".replace");
 
+    if (!outputFile.is_open()) {
+        std::cerr << "Error: could not create the output file\n";
+        return 1;
+    }
+    outputFile << replaced_content;
+    outputFile.close(); // unnecessary
     return 0;
 }
