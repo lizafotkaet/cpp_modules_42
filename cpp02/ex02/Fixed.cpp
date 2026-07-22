48cd0bd30cb282e0f0f80f9594e02c2efd7e9bc3
lizafotkaet <elizavetabarbashh@gmail.com>
2026-07-22 14:17:22 +0200

    cpp02 ex02 done

--- cpp02/ex02/Fixed.cpp
+++ cpp02/ex02/Fixed.cpp
@@ -47,66 +47,78 @@ float Fixed::toFloat(void) const{
 
 // 6 comparison operators: >, <, >=, <=, ==, and != 
 
-bool	Fixed::operator>(const Fixed& other){
-
+bool	Fixed::operator>(const Fixed& other) const{
+	return (this->value > other.value);
 }
 
-bool	Fixed::operator<(const Fixed& other){
-	
+bool	Fixed::operator<(const Fixed& other) const{
+	return (this->value < other.value);
 }
 
-bool	Fixed::operator>=(const Fixed& other){
-
+bool	Fixed::operator>=(const Fixed& other) const{
+	return (this->value >= other.value);
 }
 
-bool	Fixed::operator<=(const Fixed& other){
-	
+bool	Fixed::operator<=(const Fixed& other) const{
+	return (this->value <= other.value);
 }
 
-bool	Fixed::operator==(const Fixed& other){
-	
+bool	Fixed::operator==(const Fixed& other) const{
+	return (this->value == other.value);
 }
 
-bool	Fixed::operator!=(const Fixed& other){
-	
+bool	Fixed::operator!=(const Fixed& other) const{
+	return (this->value != other.value);
 }
 
 // 4 arithmetic operators: +, -, *, and /
 
-Fixed	Fixed::operator+(const Fixed& other){
-
+Fixed	Fixed::operator+(const Fixed& other) const{
+	return (this->value + other.value);
 }
 
-Fixed	Fixed::operator-(const Fixed& other){
-	
+Fixed	Fixed::operator-(const Fixed& other) const{
+	return (this->value - other.value);
 }
 
-Fixed	Fixed::operator*(const Fixed& other){
-	
+Fixed	Fixed::operator*(const Fixed& other) const{
+	return (this->value * other.value);
 }
 
-Fixed	Fixed::operator/(const Fixed& other){
-	
+Fixed	Fixed::operator/(const Fixed& other) const{
+	return (this->value / other.value);
 }
 
 // The 4 increment/decrement (pre-increment and post-increment, pre-decrement and
 // post-decrement) operators, which will increase or decrease the fixed-point value by
 // the smallest representable ϵ, such that 1 + ϵ > 1.
 
-Fixed&	Fixed::operator++(const int eps){
+Fixed&	Fixed::operator++(){ // prefix increment
+	++(this->value); // value += 1/256
 
+	return *this;
 }
 
-Fixed& Fixed::operator--(const int eps){
+Fixed& Fixed::operator--(){ // prefix decrement
+	--(this->value);
 
+	return *this;
 }
 
-Fixed& Fixed::--operator(const int eps){
+Fixed Fixed::operator++(int){ // postfix increment
+	Fixed temp(*this);
 
+	++(*this);
+
+	return temp;
 }
 
-Fixed& Fixed::++operator(const int eps){
-	
+Fixed Fixed::operator--(int){ // postfix decrement
+	Fixed temp(*this);
+
+	++(*this);
+
+	return temp;
 }
 
 /*
@@ -114,34 +126,31 @@ Fixed& Fixed::++operator(const int eps){
 Add these four public overloaded member functions to your class:
 • A static member function min that takes two references to fixed-point numbers as
 parameters, and returns a reference to the smallest one.
-• A static member function min that takes two references to constant fixed-point
+• A static member function min that takes two references to CONSTANT fixed-point
 numbers as parameters, and returns a reference to the smallest one.
 • A static member function max that takes two references to fixed-point numbers as
 parameters, and returns a reference to the greatest one.
-• A static member function max that takes two references to constant fixed-point
+• A static member function max that takes two references to CONSTANT fixed-point
 numbers as parameters, and returns a reference to the greatest one.
 
 */
 
 Fixed& Fixed::min(Fixed& a, Fixed& b){
-
+	return (a < b) ? a : b;
 }
-Fixed& Fixed::min(const Fixed& a, const Fixed& b){
-
+const Fixed& Fixed::min(const Fixed& a, const Fixed& b){
+	return (a < b) ? a : b;
 }
 
 Fixed& Fixed::max(Fixed& a, Fixed& b){
-
+	return (a > b) ? a : b;
 }
 
-Fixed& Fixed::max(const Fixed& a, const Fixed& b){
-
+const Fixed& Fixed::max(const Fixed& a, const Fixed& b){
+	return (a > b) ? a : b;
 }
 
-
-
-
-
+// input stream insertion operator overload
 
 std::ostream& operator<<(std::ostream& out, const Fixed& fixed){
     out << fixed.toFloat();
--- cpp02/ex02/Fixed.h
+++ cpp02/ex02/Fixed.h
@@ -19,47 +19,47 @@ class Fixed{
 
 		// 6 comparison operators: >, <, >=, <=, ==, and != 
 
-		bool operator<(const Fixed& other); // (the left operand is the implicit *this object)
-		bool operator>(const Fixed& other);
-		bool operator>=(const Fixed& other);
-		bool operator<=(const Fixed& other);
-		bool operator==(const Fixed& other);
-		bool operator!=(const Fixed& other);
+		bool operator<(const Fixed& other) const; // (the left operand is the implicit *this object)
+		bool operator>(const Fixed& other) const;
+		bool operator>=(const Fixed& other) const;
+		bool operator<=(const Fixed& other) const;
+		bool operator==(const Fixed& other) const;
+		bool operator!=(const Fixed& other) const;
 
 		// 4 arithmetic operators: +, -, *, and /
 
-		Fixed operator+(const Fixed& other);
-		Fixed operator-(const Fixed& other);
-		Fixed operator*(const Fixed& other);
-		Fixed operator/(const Fixed& other);
+		Fixed operator+(const Fixed& other) const;
+		Fixed operator-(const Fixed& other) const;
+		Fixed operator*(const Fixed& other) const;
+		Fixed operator/(const Fixed& other) const;
 
 		// The 4 increment/decrement (pre-increment and post-increment, pre-decrement and
 		// post-decrement) operators, which will increase or decrease the fixed-point value by
 		// the smallest representable ϵ, such that 1 + ϵ > 1.
 
-		Fixed& operator++(const int eps);
-		Fixed& operator--(const int eps);
-		Fixed& --operator(const int eps);
-		Fixed& ++operator(const int eps);
+		Fixed& operator++(); // prefix has no parameter
+		Fixed& operator--();
+		Fixed operator++(int); // postfix has an int parameter
+		Fixed operator--(int);
 
 		/*
 
 		Add these four public overloaded member functions to your class:
 		• A static member function min that takes two references to fixed-point numbers as
 		parameters, and returns a reference to the smallest one.
-		• A static member function min that takes two references to constant fixed-point
+		• A static member function min that takes two references to CONSTANT fixed-point
 		numbers as parameters, and returns a reference to the smallest one.
 		• A static member function max that takes two references to fixed-point numbers as
 		parameters, and returns a reference to the greatest one.
-		• A static member function max that takes two references to constant fixed-point
+		• A static member function max that takes two references to CONSTANT fixed-point
 		numbers as parameters, and returns a reference to the greatest one.
 
 		*/
 
-		Fixed& min(Fixed& a, Fixed& b);
-		Fixed& min(const Fixed& a, const Fixed& b);
-		Fixed& max(Fixed& a, Fixed& b);
-		Fixed& max(const Fixed& a, const Fixed& b);
+		static Fixed& min(Fixed& a, Fixed& b);
+		static const Fixed& min(const Fixed& a, const Fixed& b);
+		static Fixed& max(Fixed& a, Fixed& b);
+		static const Fixed& max(const Fixed& a, const Fixed& b);
 
 	private :
 		int	value;
